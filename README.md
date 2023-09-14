dac66f7b56df26f2407c877e6b83465f86ffcc43
TIAGO MARTINS RODRIGUES JOÃO <tiago_m_joao@estudante.sesisenai.org.br>
2023-09-14 08:15:46 -0300

    altercao

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub-Search
+- Utilizando o fetch APi para consumir a API do github;
+- Informações a serem requisitadas:
+- Imagem do Avatar;
+- Nome;
+- Username;
+- Quantidade de repositórios;
+- Quantidade de seguidores;
+- Quantidade de usuários que está seguindo;
---
+++ script.js
@@ -0,0 +1,23 @@
+function getProfile() {
+    event.preventDefault();
+    const username = document.getElementById('usernameInput').value;
+    fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(data => {
+        const profile = document.getElementById('profile-info');
+        if (data.message === 'Not Found') {
+          profile.innerHTML = "No profile";
+        } else {
+          profile.innerHTML = `
+            <img id="imgurl" src="${data.avatar_url}"><br> 
+            <span id="OutData">Nome: </span>${data.name}<br>
+            <span id="OutData">Nickname: </span> ${data.login}<br>
+            <span id="OutData">Quantidade de repositorios:</span> ${data.public_repos}<br>
+            <span id="OutData">Seguidores: </span> ${data.followers}<br>
+            <span id="OutData">Seguindo: </span> ${data.following}<br><span id="OutData">Repositorio Link: </span> ${data.html_url+'/?tab=repositories'}<br>
+
+          `;
+        }
+      });
+  }
+  
